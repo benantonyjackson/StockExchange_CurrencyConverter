025f775c3c22f1b1537038179d9cb518ac408a90
benantonyjackson <benantonyjackson@gmail.com>
2020-12-16 00:59:03 +0000

    Added endpoint to return all endpoints

--- CurrencyConvertorPythonAPI/api.py
+++ CurrencyConvertorPythonAPI/api.py
@@ -5,6 +5,9 @@ from flask import jsonify
 import requests
 import json
 
+BASE_URL = "https://free.currconv.com"
+API_KEY = "809e6007e713ca1b8fd6"
+
 app = Flask(__name__)
 
 
@@ -18,6 +21,15 @@ def get_default_response(body={}):
 @app.route("/convert", methods=["GET"])
 def convert():
     return get_default_response({"Message": "It worked"})
+
+
+@app.route("/get_all_currencies")
+def get_all_currencies():
+    res = requests.get(BASE_URL + "/api/v7/currencies", params={'apiKey': API_KEY})
+
+    list_of_currency_codes = json.loads(res.text)
+
+    return get_default_response({"all_currencies": list(list_of_currency_codes['results'].keys())})
 
 
 if __name__ == "__main__":
