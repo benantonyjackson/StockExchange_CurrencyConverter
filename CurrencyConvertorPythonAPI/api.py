e336823662322334ea2a310ac123fd45b502e997
benantonyjackson <benantonyjackson@gmail.com>
2020-12-19 23:29:19 +0000

    Switched to different API and added function to save response to file

--- CurrencyConvertorPythonAPI/api.py
+++ CurrencyConvertorPythonAPI/api.py
@@ -5,10 +5,17 @@ from flask import jsonify
 import requests
 import json
 
-BASE_URL = "https://free.currconv.com"
-API_KEY = "809e6007e713ca1b8fd6"
+BASE_URL = "http://data.fixer.io/api"
+API_KEY = "ad056783a6a38055946311f0eb3a79e2"
+BASE_CURRENCY = 'EUR'
 
 app = Flask(__name__)
+
+
+def save_rates_to_file(data, file_name="rates.json"):
+    file = open(file_name, "w")
+
+    file.write(json.dumps(data))
 
 
 def get_default_response(body={}):
@@ -18,31 +25,16 @@ def get_default_response(body={}):
     return res
 
 
-def construct_query_parameter():
-    all_currencies = json.loads(get_all_currencies().response)['all_currencies']
-
-    string = ""
-    params = {"compact": "ultra", "apiKey": API_KEY}
-
-    base_currency = "GBP"
-
-    for currency in all_currencies:
-        string += base_currency + "_" + currency + ','
-
-    params["q"] = string[:-1]
-
-    return params
-
-
 @app.route("/convert", methods=["GET"])
 def convert():
-    return str(construct_query_parameter())
-
+    res = requests.get(BASE_URL + "/latest", params={'access_key': API_KEY})
+    save_rates_to_file(res.text)
+    return Response(res.text, headers={"Content-type": "application/json"})
 
 
 @app.route("/get_all_currencies")
 def get_all_currencies():
-    res = requests.get(BASE_URL + "/api/v7/currencies", params={'apiKey': API_KEY})
+    res = requests.get(BASE_URL + "/symbols", params={'access_key': API_KEY})
 
     list_of_currency_codes = json.loads(res.text)
 
